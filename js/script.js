e9db57d2b6d8e1744629a2429d51ad2b0835e699
Kanatmr <97095210+Kanatmr@users.noreply.github.com>
2022-02-25 08:40:34 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+var body=document.querySelector("body"),
+nav = document.querySelector("nav");
+modeToggle = document.querySelector(".dark-light"),
+searchToggle = document.querySelector(".searchToggle"),
+sidebarClose = document.querySelector(".sidebarClose"),
+sidebarOpen = document.querySelector(".sidebarOpen");
+
+modeToggle.addEventListener("click" , () =>{
+  modeToggle.classList.toggle("active")
+  body.classList.toggle("dark")
+})
+searchToggle.addEventListener("click" , () =>{
+  searchToggle.classList.toggle("active")
+})
+sidebarOpen.addEventListener("click", () =>{
+  nav.classList.add("active");
+})
+body.addEventListener("click", e =>{
+  let clickedElm=e.target;
+  if(!clickedElm.classList.contains("sidebarOpen") && (!clickedElm.classList.contains("menu"))){
+    nav.classList.remove("active");
+  }
+})
+
+
+
+
+$(".but").click(function(){
+  var audio = new Audio("b.mp3");
+  audio.play();
+})
